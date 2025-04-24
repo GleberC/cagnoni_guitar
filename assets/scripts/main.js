03378e22f5a3a8449191a05d4fb8364ef4076573
GleberC <gleber10@yahoo.com.br>
2025-04-24 10:59:42 -0300

    button scroll

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,8 @@ const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modal-image');
 const modalDesc = document.getElementById('image-description');
 const closeModal = document.getElementById('close-modal');
+// Pega o botão
+const scrollTopBtn = document.getElementById('scroll-top-btn');
 
 thumbnails.forEach(img => {
   img.addEventListener('click', () => {
@@ -22,3 +24,27 @@ window.addEventListener('click', (e) => {
     modal.style.display = 'none';
   }
 });
+
+// Mostrar o botão quando o usuário rolar a página
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > 200) {  // Muda 200 para a distância que achar ideal
+//     scrollTopBtn.style.display = 'block';
+//   } else {
+//     scrollTopBtn.style.display = 'none';
+//   }
+// });
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+      scrollTopBtn.classList.add('show');
+    } else {
+      scrollTopBtn.classList.remove('show');
+    }
+  });
+
+
+
+// Função de rolagem para o topo
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });  // Rolagem suave para o topo
+});
+
